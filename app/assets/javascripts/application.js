0391529cde82d0b36dae573c67434af193d858fd
jjmuteteke <jjmuteteke@yahoo.com>
2016-03-12 21:39:43 +0000

    testap3
    Signed-off-by: jjmuteteke <jjmuteteke@yahoo.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
-//= require jquery.cookie
+//= require js.cookie
 //= require private_pub
 //= require chat
 //= require turbolinks
