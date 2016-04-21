68df5d159e891a38f745a8f90e798dd01743561a
jjmuteteke <jjmuteteke@yahoo.com>
2016-04-21 05:19:43 +0000

    test14
    Signed-off-by: jjmuteteke <jjmuteteke@yahoo.com>

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'Oregon',
-  credentials: Aws::Credentials.new(ENV['AKIAJTXU6ODJFSNWQ5VA'], ENV['DNf3X97Imm+cBIE8hHHTLiFHidlpnl6K6wnG+6qO']),
+  credentials: Aws::Credentials.new(ENV['AKIAJTXU6ODJFSNWQ5VA'], ENV['DNf3X97Imm+cBIE8hHHTLiFHidlpnl6K6wnG+6qO'])
 })
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['groupchat'])
