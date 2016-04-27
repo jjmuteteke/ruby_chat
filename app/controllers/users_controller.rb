be2d3b2e2e2a58af1c311cb0e926cfa55caba538
jjmuteteke <jjmuteteke@yahoo.com>
2016-04-27 05:40:59 +0000

    test112
    Signed-off-by: jjmuteteke <jjmuteteke@yahoo.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   def index
       @fusers = User.all
       @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
-      @conversations = Conversation.involving(current_user).order("created_at DESC")
-      @groupconversations = Groupconversation.joins(:users).where(:users => {:id => current_user.id}).order("created_at DESC")
+      @co = Conversation.involving(current_user).order("created_at DESC")
+      @gco = Groupconversation.joins(:users).where(:users => {:id => current_user.id}).order("created_at DESC")
       @assets = Asset.involving(current_user)
   end
 end
